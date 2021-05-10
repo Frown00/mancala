718d2c42b6d17ef059eb79c950bb5cdb77010b4e
Fabian Kuriata <f.kuriata@t-bull.com>
2021-05-11 00:15:49 +0200

    style(game): simplify code - split into smaller functions

--- src/game/mechanics/MankalaGame.ts
+++ src/game/mechanics/MankalaGame.ts
@@ -21,14 +21,7 @@ export class MankalaGame {
   constructor(startingStones: number) {
     const holesPerPlayer = 6;
     this.history = [];
-    this.state = [
-      { player: Player._1, stones: [], points: 0 },
-      { player: Player._2, stones: [], points: 0 }
-    ];
-    for(let i = 0; i < holesPerPlayer; i++) {
-      this.state[0].stones.push(startingStones);
-      this.state[1].stones.push(startingStones);
-    }
+    this.spreadStones(holesPerPlayer, startingStones);
     const drawTurn = Math.floor(Math.random() * 2);
     this.playerTurn = Object.values(Player)[drawTurn];
   }
@@ -39,67 +32,14 @@ export class MankalaGame {
 
   turn(holeId: number): { status: GameStatus, isCaptured: boolean } {
     this.history.push({ states: _.cloneDeep(this.state), turn: this.playerTurn });
-    const playerState = this.state.find(s => s.player === this.playerTurn);
-    const enemyState = this.state.find(s => s.player !== this.playerTurn);
-    const player = this.playerTurn;
-    const holeIdx = holeId - 1;
-    const holes = playerState.stones.length;
-    let stones = playerState.stones[holeIdx];
-    playerState.stones[holeIdx] = 0;
-    let currentHole = holeId;
-    let isEnemyState = false;
-    let currentState = playerState;
-    let isAnotherTurn = false;
-    while(stones > 0) {
-      if(currentHole >= holes) {
-        if(isEnemyState) {
-          currentState = playerState;
-        } else {
-          stones--;
-          currentState.points++;
-          if(stones === 0) 
-            isAnotherTurn = true;
-          else 
-            currentState = enemyState;
-        }
-        currentHole = 0;
-        isEnemyState = !isEnemyState;
-      } else {
-        stones--;
-        currentState.stones[currentHole] += 1;
-        currentHole++;
-      }
-    }
+    const takingAction = this.playerTurn;
+    const { isAnotherTurn, currentState } = this.moveCounterclockwise(holeId, takingAction);
+    const isCaptured = this.captureStones(currentState, takingAction);
     if(!isAnotherTurn) {
       this.playerTurn = this.playerTurn === Player._1 ? Player._2 : Player._1;
     }
-    this.lastTouchedHole = currentHole - 1;
-    // isCapture
-    let isCaptured = false;
-    if(currentState.player === player) {
-      const id = this.lastTouchedHole;
-      const stones = currentState.stones[id];
-      const enemyStones = enemyState.stones[holes - currentHole]
-      if(stones === 1 && enemyStones > 0) {
-        const earned = enemyStones + stones;
-        enemyState.stones[holes - currentHole] = 0;
-        currentState.stones[id] = 0;
-        currentState.points += earned;
-        console.log('CAPTURED', earned);
-        isCaptured = true;
-      }
-    }
-    // isEnd
-    if(playerState.stones.every(v => v === 0) || enemyState.stones.every(v => v === 0)) {
-      const enemyRest = _.sum(enemyState.stones);
-      const playerRest = _.sum(playerState.stones);
-      enemyState.stones.fill(0);
-      playerState.stones.fill(0);
-      enemyState.points += enemyRest;
-      playerState.points += playerRest;
-      return { status: GameStatus.FINISHED, isCaptured };
-    }
-    return { status: GameStatus.PLAYING, isCaptured };
+    const status = this.isGameOver(takingAction);
+    return { status, isCaptured };
   }
 
   undo() {
@@ -129,4 +69,86 @@ export class MankalaGame {
   getLastTouched(): number {
     return this.lastTouchedHole;
   }
+
+  private spreadStones(holesPerPlayer: number, startingStones: number) {
+    this.state = [
+      { player: Player._1, stones: [], points: 0 },
+      { player: Player._2, stones: [], points: 0 }
+    ];
+    for(let i = 0; i < holesPerPlayer; i++) {
+      this.state[0].stones.push(startingStones);
+      this.state[1].stones.push(startingStones);
+    }
+  }
+
+  private moveCounterclockwise(holeId: number, takingAction: Player) {
+    const playerState = this.state.find(s => s.player === takingAction);
+    const enemyState = this.state.find(s => s.player !== takingAction);
+    let isEnemyState = false;
+    const idx = holeId - 1;
+    let stones = playerState.stones[idx];
+    let currentHole = holeId;
+    let currentState = playerState;
+    let isAnotherTurn = false;
+    const holes = playerState.stones.length;
+    playerState.stones[idx] = 0;
+    while(stones > 0) {
+      if(currentHole >= holes) {
+        if(isEnemyState) {
+          currentState = playerState;
+        } else {
+          stones--;
+          currentState.points++;
+          if(stones === 0) 
+            isAnotherTurn = true;
+          else 
+            currentState = enemyState;
+        }
+        currentHole = 0;
+        isEnemyState = !isEnemyState;
+      } else {
+        stones--;
+        currentState.stones[currentHole] += 1;
+        currentHole++;
+      }
+    }
+    this.lastTouchedHole = currentHole - 1;
+    return { isAnotherTurn, currentState };
+  }
+
+  private captureStones(playerState: PlayerState, takingAction: Player) {
+    if(playerState.player !== takingAction) {
+      return false;
+    }
+    const enemyState = this.state.find(s => s.player !== takingAction);
+    const holes = playerState.stones.length;
+    const id = this.lastTouchedHole;
+    const stones = playerState.stones[id];
+    const enemyStones = enemyState.stones[holes - 1 - id];
+    if(stones === 1 && enemyStones > 0) {
+      const earned = enemyStones + stones;
+      enemyState.stones[holes - 1 - id] = 0;
+      playerState.stones[id] = 0;
+      playerState.points += earned;
+      console.log('CAPTURED', earned);
+      return true;
+    }
+    return false;
+  }
+
+  private isGameOver(takingAction: Player) {
+    const playerState = this.state.find(s => s.player === takingAction);
+    const enemyState = this.state.find(s => s.player !== takingAction);
+    if(playerState.stones.every(v => v === 0) || enemyState.stones.every(v => v === 0)) {
+      const enemyRest = _.sum(enemyState.stones);
+      const playerRest = _.sum(playerState.stones);
+      enemyState.stones.fill(0);
+      playerState.stones.fill(0);
+      enemyState.points += enemyRest;
+      playerState.points += playerRest;
+      return GameStatus.FINISHED;
+    }
+    return GameStatus.PLAYING;
+  }
+
 }
